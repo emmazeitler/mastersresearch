5136f6d196a41b8946666899dc1d46dbb3ea46a5
Emma Zeitler <ezeitler@ufl.edu>
2022-10-28 10:53:34 -0400

    Some histograms

--- 03_Scripts/DBEnv_Exploratory.R
+++ 03_Scripts/DBEnv_Exploratory.R
@@ -13,3 +13,21 @@ db_data <- read.csv("02_Clean_data/Dungbeetle_Env_clean.csv")
 db_data$block_id <- as.factor(db_data$block_id)
 db_data$burn_season <- as.factor(db_data$burn_season)
 db_data$env_type <- as.factor(db_data$env_type)
+
+# Filter NAs
+db_data$latency2 = gsub("NA", "", db_data$latency) %>% as.numeric()
+db_data$hour2 = gsub("NA", "", db_data$hour) %>% as.numeric()
+
+remno_adj <- db_data %>% 
+  select(date, block_id, burn_season, env_type, rem_no) %>% 
+  filter(rem_no > 0)
+
+#Histograms 
+ggplot(db_data, aes(x = rem_event)) + 
+  geom_histogram()
+
+ggplot(db_data, aes(x = rem_no)) +
+  geom_histogram() 
+
+ggplot(remno_adj, aes(x = rem_no)) +
+  geom_histogram() 
